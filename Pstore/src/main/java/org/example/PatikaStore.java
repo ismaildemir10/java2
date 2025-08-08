361ab75d478bc20c8b522a458075908f6b20e9d5
ICD <iscdr10@gmail.com>
2025-08-08 04:43:05 +0300

    pstore

--- Pstore/src/main/java/org/example/PatikaStore.java
+++ Pstore/src/main/java/org/example/PatikaStore.java
@@ -11,18 +11,18 @@ public class PatikaStore {
         ArrayList<Notebook> notebookList = new ArrayList<>();
         ArrayList<Telefon> telefonList = new ArrayList<>();
 
-        // 📦 Notebook ürünleri
+        //  Notebook ürünleri
         notebookList.add(new Notebook(101, "Lenovo ThinkPad", 32000, "Lenovo", 512, 14.0, 16));
         notebookList.add(new Notebook(102, "Apple MacBook Air", 45000, "Apple", 256, 13.3, 8));
         notebookList.add(new Notebook(103, "Monster Tulpar", 39000, "Monster", 1024, 15.6, 32));
 
-        // 📱 Telefon ürünleri
+        //  Telefon ürünleri
         telefonList.add(new Telefon(201, "Samsung Galaxy S23", 38000, "Samsung", 128, 6.1, 4500, 8, "Siyah"));
         telefonList.add(new Telefon(202, "iPhone 14", 52000, "Apple", 256, 6.1, 3200, 6, "Mavi"));
         telefonList.add(new Telefon(203, "Xiaomi Redmi Note", 21000, "Xiaomi", 128, 6.5, 5000, 6, "Kırmızı"));
 
         while (true) {
-            System.out.println("\n📦 PatikaStore");
+            System.out.println("\n PatikaStore");
             System.out.println("1 - Notebookları Listele");
             System.out.println("2 - Telefonları Listele");
             System.out.println("3 - Çıkış");
